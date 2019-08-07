a569ad3e18f378376c8689e6a672f80fa4661bc8
hooheohee <45270575+hooheohee@users.noreply.github.com>
2019-08-07 13:20:22 -0400

    basic route error handling

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,11 @@
+import { LoggingInterceptor } from './shared/logging.interceptor';
+import { HttpErrorFilter } from './shared/http-error.filter';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdeaModule } from './idea/idea.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -19,6 +22,16 @@ import { IdeaModule } from './idea/idea.module';
     IdeaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
