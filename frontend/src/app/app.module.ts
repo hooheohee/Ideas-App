dc89ca962f28bf940d2eb3ba76c64e3e719ccc21
hooheohee <45270575+hooheohee@users.noreply.github.com>
2019-08-15 16:52:12 -0400

    Initiate frontend - angular/ngrx

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+
+import { AppRoutingModule } from "@app/app-routing.module";
+import { AppComponent } from "@app/app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
