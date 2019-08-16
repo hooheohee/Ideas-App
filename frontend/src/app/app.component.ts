3a1bc1805efaaad0d02fe8847bcacc37127c3cf6
hooheohee <45270575+hooheohee@users.noreply.github.com>
2019-08-16 14:27:31 -0400

    add ngrx store

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "./store/app-store.module";
+import { AddError } from "./store/actions/errors.action";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "ideas-app-frontend";
+
+  // constructor(private store: Store<AppState>) {}
+
+  // ngOnInit() {
+  //   this.store.dispatch(new AddError({ error: "message!" }));
+  // }
 }
---
+++ frontend/src/app/store/app-store.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StoreModule, ActionReducerMap } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
+import { errorReducer, ErrorState } from "./reducers/errors.reducer";
+
+export interface AppState {
+  error: ErrorState;
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  error: errorReducer
+};
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument(),
+    EffectsModule.forRoot([])
+  ]
+})
+export class AppStoreModule {}
